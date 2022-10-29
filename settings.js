a29232b71eb9bc0b059c84f125273dd3bcb93bf6
queenreemah <108387167+ashenthebot@users.noreply.github.com>
2022-10-29 20:19:58 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.vcardowner = ['94784749430'] //ur owner number
+global.ownername = "🎀 𝒬𝓊𝑒𝑒𝓃 𝑅𝑒𝑒𝓂𝒶𝒽 🎀 " //ur owner name
+global.ytname = "YT: A̳s̳h̳e̳n̳ ̳R̳i̳d̳e̳r̳" //ur yt chanel name
+global.socialm = "GitHub: ̳queenreemah" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
+global.owner = ['94784749430']
+global.ownertag = '94784749430' //ur tag number
+global.botname = '🎀 𝒬𝓊𝑒𝑒𝓃 𝑅𝑒𝑒𝓂𝒶𝒽 🎀 ' //ur bot name
+global.linkz = "https://www.youtube.com/channel/UC2PmyEBQcSb0yJs4JUi4zqA" //your theme url which will be displayed on whatsapp
+global.websitex = "https://www.youtube.com/channel/UC2PmyEBQcSb0yJs4JUi4zqA" //ur website to be displayed
+global.botscript = 'https://github.com/ashenthebot' //script link
 global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.themeemoji = "💥" //ur theme emoji
+global.packname = "@𝖗𝖎𝖉𝖊𝖗𝖇𝖔𝖙1 " //ur sticker watermark packname
+global.author = "A̳s̳h̳e̳n̳ ̳R̳i̳d̳e̳r̳" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['94784749430'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
